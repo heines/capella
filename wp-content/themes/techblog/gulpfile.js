a92e20b22ad04e05913bb858e292654d33f20657
heine <heine.shimizu@gmail.com>
2018-08-24 20:09:36 +0900

    自宅作業のため、コミットに足る状態ではないがブランチにデータを退避する。

--- wp-content/themes/techblog/gulpfile.js
+++ wp-content/themes/techblog/gulpfile.js
@@ -3,11 +3,15 @@ var notify = require("gulp-notify");
 var plumber = require("gulp-plumber");
 var pug = require('gulp-pug');
 var browserSync = require("browser-sync");
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
 
 //setting : paths
 var paths = {
   'pug': './src/pug/',
-  'html': '.',
+  'html': './src/dest/',
+  'sass': './src/scss/',
+  'css': './src/dest/css/'
 }
 
 //setting : Pug Options
@@ -20,10 +24,14 @@ gulp.task('pug', () => {
   return gulp.src([paths.pug + '**/*.pug', '!' + paths.pug + '**/_*.pug'])
     .pipe(plumber({ errorHandler: notify.onError("Error: <%= error.message %>") }))
     .pipe(pug(pugOptions))
-    .pipe(rename({
-      extname: '.php'
-    }))
     .pipe(gulp.dest(paths.html));
+});
+
+gulp.task('sass', () => {
+  return gulp.src([paths.sass + '**/*.scss', '!' + paths.sass + '**/_*.scss'])
+    .pipe(plumber({ errorHandler: notify.onError("Error: <%= error.message %>") }))
+    .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(gulp.dest(paths.css));
 });
 
 //Browser Sync
@@ -31,10 +39,11 @@ gulp.task('browser-sync', () => {
   browserSync({
     server: {
       baseDir: paths.html,
-      index: "index.php"
-
+      index: "front-page.html"
     }
   });
+  gulp.watch(paths.html + "**/*.html", ['reload']);
+  gulp.watch(paths.css + "**/*.css", ['reload']);
 });
 gulp.task('reload', () => {
   browserSync.reload();
@@ -42,6 +51,7 @@ gulp.task('reload', () => {
 
 //watch
 gulp.task('watch', function () {
+  gulp.watch([paths.sass + '**/*.scss', '!' + paths.sass + '**/_*.scss'],['sass']);
   gulp.watch([paths.pug + '**/*.pug', '!' + paths.pug + '**/_*.pug'],['pug', 'reload']);
 });
 
