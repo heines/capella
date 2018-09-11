813269d48b794269788aa1cd392d7a98b8b53346
heine <heine.shimizu@gmail.com>
2018-09-11 14:23:26 +0900

    タグテンプレートファイルとカテゴリテンプレートファイルを追加

---
+++ wp-content/themes/techblog/category.php
@@ -0,0 +1,22 @@
+<!-- header -->
+<html lang="ja">
+<head>
+  <?php get_header(); ?>
+</head>
+  <body>
+    <canvas id="canvas">
+      <script type="text/javascript" src="/wp/wp-content/themes/techblog/bgwave.js"></script>
+    </canvas>
+    <div id="mainbody">
+      <div class="mainbox">
+        <?php get_sidebar(); ?>
+        <div class="contentsbox">
+        </div>
+      </div>
+    </div>
+    <!-- footer -->
+    <footer>
+      <div class="tests"></div>
+    </footer><?php get_footer(); ?>
+  </body>
+</html>
