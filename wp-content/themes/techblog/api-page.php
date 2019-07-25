9af889bf7403389338158425587f15fb82b05f6e
清水園子 <s-shimizu@tsumikiinc.com>
2019-07-25 21:24:13 +0900

    リクエストパラメータを使用してAPIを実行する処理を追加

--- wp-content/themes/techblog/api-page.php
+++ wp-content/themes/techblog/api-page.php
@@ -5,11 +5,12 @@ Template Name: API出力
 ?>
 <?php
 $base_url = 'http://zipcloud.ibsnet.co.jp/api/search';
-if(isset($_GET['param'])) {
-  $param = $_GET['param'];
+if(isset($_GET['code'])) {
+  $code = $_GET['code'];
+} else {
+  $code = '1050011';
 }
-$testcode = '1050011';
-$response = file_get_contents($base_url.'?zipcode='.$testcode);
+$response = file_get_contents($base_url.'?zipcode='.$code);
 $json = json_decode($response,JSON_UNESCAPED_UNICODE);
 header("Content-Type: application/json; charset=utf-8");
 echo json_encode($json);
