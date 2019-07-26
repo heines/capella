278e20c83294307a8169a50fbfd0a532635c1a82
清水園子 <s-shimizu@tsumikiinc.com>
2019-07-26 13:54:47 +0900

    headerにACAOを追加

--- wp-content/themes/techblog/api-page.php
+++ wp-content/themes/techblog/api-page.php
@@ -13,5 +13,6 @@ if(isset($_GET['code'])) {
 $response = file_get_contents($base_url.'?zipcode='.$code);
 $json = json_decode($response,JSON_UNESCAPED_UNICODE);
 header("Content-Type: application/json; charset=utf-8");
+header("Access-Control-Allow-Origin: https://heines.github.io");
 echo json_encode($json);
 ?>
